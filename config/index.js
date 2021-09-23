4eb85cb64ae91010354d203c05aaa4a4b75c388c
Bueggi <christopher_b@gmx.de>
2021-09-23 21:05:48 +0200

    changed NEXT_CLIENT_URL

--- config/index.js
+++ config/index.js
@@ -1,2 +1,4 @@
 export const API_URL = process.env.API_URL || "https://secure-escarpment-39746.herokuapp.com"
 export const NEXT_CLIENT_URL = process.env.NEXT_CLIENT_URL || "https://secure-escarpment-39746.herokuapp.com/"
+
+console.log(process.env)
