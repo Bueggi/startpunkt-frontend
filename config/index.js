cebf5dcb38851a8eecfde87084f44c7a0734d3d7
Bueggi <christopher_b@gmx.de>
2021-09-23 21:12:41 +0200

    changed NEXT_PUBLIC_CLIENT in config/index

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 export const API_URL = process.env.API_URL || "https://secure-escarpment-39746.herokuapp.com"
-export const NEXT_CLIENT_URL = process.env.NEXT_CLIENT_URL || "https://secure-escarpment-39746.herokuapp.com/"
+export const NEXT_PUBLIC_CLIENT_URL = process.env.NEXT_CLIENT_URL || "https://startpunktfrontend-flax.vercel.app"
 
 console.log(process.env)
