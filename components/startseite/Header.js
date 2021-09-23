b55290c44edc7002777686c5a7fa81f3402be1ad
Bueggi <christopher_b@gmx.de>
2021-09-23 11:26:00 +0200

    added button in navigation that leads to dashboard

--- components/startseite/Header.js
+++ components/startseite/Header.js
@@ -124,6 +124,13 @@ export default function Example() {
                             <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900" onClick={() => logout()}>
                                 Logout
                             </a>
+                            <Link href='/app'>
+                                <a
+                                    className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-green-600 hover:bg-green-700"
+                                >
+                                    Zu den Kursen
+                                </a>
+                            </Link>
                         </div>}
                 </div>
             </div>
